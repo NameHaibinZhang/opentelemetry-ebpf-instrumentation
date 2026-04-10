d25363dbbf415278d0fcbb796338f7bf15f269e3
Mario Macias <mario.macias@grafana.com>
2026-04-10 16:07:49 +0200

    Fix OBI being blocked on shutdown (#1782)
    * Fix OBI being blocked on shutdown
    
    * extend ktraces test timeout

--- internal/test/integration/components/gogeneric/main.go
+++ internal/test/integration/components/gogeneric/main.go
@@ -14,7 +14,6 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
